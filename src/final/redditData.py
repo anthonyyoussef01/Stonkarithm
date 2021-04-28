2ef43eb6bbd4a677e7cede28d9645efdfb03a938
anthonyyoussef01 <30884253+anthonyyoussef01@users.noreply.github.com>
2021-04-28 10:33:28 -0400

    Update redditData.py

--- src/final/redditData.py
+++ src/final/redditData.py
@@ -1,15 +1,4 @@
-import praw
 import requests
-
-
-# reddit = praw.Reddit(
-#     client_id="VZ3OOz_XrxLXBw",
-#     client_secret="Gzs5E25-FDqPHQ7F2qDFHf-jBJ72lA",
-#     username="Puzzleheaded_Row_596",
-#     password="cs4100project!",
-#     user_agent="testscript by josh",
-# )
-
 
 def getRedditPosts(from_date=str, to_date=str, query=str):
     str1 = "https://api.pushshift.io/reddit/submission/search/?size=100&q="
@@ -26,10 +15,3 @@ def getRedditPosts(from_date=str, to_date=str, query=str):
     for post in source['data']:
         list_of_text.append(post['title'].encode('utf-16', 'surrogatepass').decode('utf-16'))
     return list_of_text
-
-"""
-from datetime import datetime
-t = 1523290473.0
-dt = datetime.fromtimestamp(t)
-print(dt)
-"""
